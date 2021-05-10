46be5d67dc4609671b168ff53db10f95a6c55b34
Oto Fargas <xfargas@fi.muni.cz>
2021-05-10 17:52:55 +0200

    changed random date in creating test data

--- app/gener/create_test_data.py
+++ app/gener/create_test_data.py
@@ -7,21 +7,11 @@ import sys
 
 import psycopg2
 
-# create function accepting a single parameter, the year as a four digit number
-def get_random_date(year):
-    # try to get a date
-    try:
-        return datetime.datetime.strptime('{} {}'.format(random.randint(1, 366), year), '%j %Y').date()
-    # if the value happens to be in the leap year range, try again
-    except ValueError:
-        return get_random_date(year)
-
 def read_file(file_name):
     file = open(file_name, 'r')
     lines = file.readlines()
     return ["{}".format(line.strip()) for line in lines]
 
-print()
 
 def generate_data():
     names = read_file(f'{os.path.dirname(os.path.realpath(__file__))}/names.txt')
@@ -32,14 +22,14 @@ def generate_data():
     with open (f'{os.path.dirname(os.path.realpath(__file__))}/data.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["ID", "first name", "second name", "email", "ui_lang", "ui_settings", "employee_account", "access_rights","logon_status","logon_last_modif"])
+        date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        for i in range(100):
+        for i in range(10000):
             name = random.choice(names)
             last_name = random.choice(last_names)
             email = name.lower() + "." + last_name.lower() + str(random.randint(0,1000)) + "@gmail.com"
             lang = random.choice(languages)
-            random_date = get_random_date(2020)
-            writer.writerow([i, name, last_name, email, lang, "default", random.choice([True, False]), "complete access", 2, random_date])
+            writer.writerow([i, name, last_name, email, lang, "default", random.choice([True, False]), "complete access", 2, date])
 
 
 def populate_table():
@@ -75,3 +65,5 @@ def populate_table():
     finally:
         if conn is not None:
             conn.close()
+
+generate_data()
