939153c005ab700949fabc95b1c367b14ad3ea5f
Vladimír Višňovský <80093856+VladimirVisnovsky@users.noreply.github.com>
2021-05-11 12:19:48 +0200

    Update refresh_table.py

--- app/refresh_table.py
+++ app/refresh_table.py
@@ -8,7 +8,7 @@ def refresh():
     cur = conn.cursor()
     current_time = datetime.datetime.timestamp(datetime.datetime.now())
 
-    cur.execute("UPDATE test_user AS tu SET logon_status = 0 WHERE tu.logon_last_modif::float < %s + 60 ", [current_time])
+    cur.execute("UPDATE test_user AS tu SET logon_status = 0 WHERE tu.logon_last_modif::float < %s - 60 ", [current_time])
 
     cur.close()
     conn.commit()
