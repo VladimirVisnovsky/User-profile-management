baad8e126a9b634b95a60f28977473c75c4b29f4
Oto Fargas <xfargas@fi.muni.cz>
2021-05-12 19:32:40 +0200

    added tests for deleting one row

--- app/performance_tests.py
+++ app/performance_tests.py
@@ -9,15 +9,29 @@ CLIENT = MongoClient("mongodb://localhost:27017")
 
 def timeit_run_tests():
 
-    print(timeit.timeit('select_all_test_postgre()', 'from __main__ import select_all_test_postgre', number=1))
-    print(timeit.timeit('select_all_test_nosql()', 'from __main__ import select_all_test_nosql', number=1))
 
-def delete_multiple_rows_test_nosql():
+    elapsed_time = timeit.timeit('delete_one_row_test_postgre()', 'from __main__ import delete_one_row_test_postgre', number=1)
+    print(elapsed_time)
+    elapsed_time = timeit.timeit('delete_one_row_test_nosql()', 'from __main__ import delete_one_row_test_nosql', number=1)
+    print(elapsed_time)
+
+    elapsed_time = timeit.timeit('delete_multiple_rows_test_postgre()', 'from __main__ import delete_multiple_rows_test_postgre', number=1)
+    print(elapsed_time)
+    elapsed_time = timeit.timeit('delete_multiple_rows_test_nosql()', 'from __main__ import delete_multiple_rows_test_nosql', number=1)
+    print(elapsed_time)
+
+
+def delete_one_row_test_postgre():
+    conn = db.get_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM test_user WHERE id='1';")
+    cur.close()
+    conn.commit()
+
+def delete_one_row_test_nosql():
     coll = nosql_db.get_collection(CLIENT)
-    query = {"first_name": {"$regex": "[a-zA-Z]*u[a-zA-Z]*"}}
+    query = {"id": {"$regex": "1"}}
     nosql_db.delete(coll, query)
-
-
 
 def delete_multiple_rows_test_postgre():
     conn = db.get_connection()
@@ -25,6 +39,13 @@ def delete_multiple_rows_test_postgre():
     cur.execute("DELETE FROM test_user WHERE first_name LIKE '%o%';")
     cur.close()
     conn.commit()
+
+def delete_multiple_rows_test_nosql():
+    coll = nosql_db.get_collection(CLIENT)
+    query = {"first_name": {"$regex": "[a-zA-Z]*u[a-zA-Z]*"}}
+    nosql_db.delete(coll, query)
+
+
 
 
 timeit_run_tests()
